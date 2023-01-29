d75255837980e27dc10daabf807b7b6c01f2e3b3
knkp <knkp934@gmail.com>
2023-01-29 18:20:52 -0500

    added example stuff for client

--- client.js
+++ client.js
@@ -5,6 +5,12 @@ socket.on('connection-gotten', () => {
   console.log('yay we connected!');
 })
 
+socket.on('got-message', message => {
+  console.log(message);
+})
+
+// TODO: when the user enters a message string and clicks "enter" do this to send the message:
+// socket.emit('send-message', message);
 
 
 
