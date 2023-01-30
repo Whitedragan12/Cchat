6f686d2b52fc49db88eee19efe813d8608d94ee2
knkp <knkp934@gmail.com>
2023-01-29 19:19:23 -0500

    server now rebroadcasts message received from a client to all clients

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const CChatServer = new Server({ /* options */ });
 CChatServer.on("connection", (socket) => {
   console.log('got a new connection!');
   socket.emit('connection-gotten');
+  socket.on("send-message", message => {
+    CChatServer.sockets.emit("got-message", message);
+  });
 });
+
 
 CChatServer.listen(3000);
